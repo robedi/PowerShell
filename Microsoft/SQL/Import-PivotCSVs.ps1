68b2145449a778b8d87be445a686ce206945ebac
RoBeDi <63212787+RoBeDi@users.noreply.github.com>
2025-07-04 10:00:31 +0200

    Update Import-PivotCSVs.ps1

--- Microsoft/SQL/Import-PivotCSVs.ps1
+++ Microsoft/SQL/Import-PivotCSVs.ps1
@@ -28,7 +28,7 @@
 
         Runs the script against the default-configured folder and SQL instance.  
         On first run, drops any existing pivot tables and imports all CSVs.  
-        On later runs, only re-imports files that have changed, updating the metadata log.
+        On later runs, imports new files and only re-imports files that have changed, updating the metadata log.
 
     .INPUTS
         None.  The script does not accept pipeline input.
